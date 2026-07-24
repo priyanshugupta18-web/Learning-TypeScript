5621160d38fcee7846338a911c8a7c4bfdf524a5
Priyanshu <priyanshuguptawebdev@gmail.com>
2026-07-24 22:51:39 +0530

    Added some content

---
+++ src/typesInTS.ts
@@ -0,0 +1,24 @@
+// type inferencing: automatically infers the type of a declared variable.
+// examples:-
+let topic = "TypeScript"
+let learner = "Priyanshu"
+let status = Math.random()*1000 > 400?"success":undefined
+
+// let status: string | undefined
+
+// inferred type: string
+
+// topic = 25  ❌ type "number" is not assignable to type "string"
+
+// type annotation: specifically telling TS that this is going to be the type of the variable
+
+// examples:-
+
+let data:object = [];
+
+let fact:string = "I am good at JavaScript"
+
+// fact = 35    ❌ type "number" is not assignable to type "string"
+// data = 34    ❌ type "number" is not assignable to type "object"  
+
+
---
+++ src/unionAndany.ts
@@ -0,0 +1,36 @@
+// union : aap koi variable declare karte time ye batate ho TypeScript ki ye variable in in possible Data types ko hold kar sakta hai...is baat ko hum union ke madhyam se samne rakh skate hai
+
+// example:-
+
+let value: string | number;
+
+// value = 25 ✅
+// value = "twenty-five" ✅
+
+// Infact you can you your own custom Data-types
+
+let risk: "low" | "medium" | "high" ;
+
+// risk = "low" either of low medium or high will work
+
+// risk = 25  ❌ type 25 is not assignable to type "low" | "medium" | "high"
+
+// any: jab hamare TS compiler ko pata nahi hota hai ki hm ek declared variable me kis tarah ke value dalenge ya yadi ham khud hi isbaare me sure nahi hai😅 tab Compiler uss variable ka type any infer karta hai...type "any" ko hamesha avoid karna chaiye as much as possible 
+
+// agar kisi variable ko declare karne ke baad usme assignment guarented nahi hai tab usko ek undefined type dena chaiye 
+
+let uncertain;
+
+let numbers = [25, 12, 10, 1]
+
+for(let num of numbers) {           //here the assignment is conditional, and hence not guaranted...I mean the array could possibly be empty...for such cases we should use undefined
+    if(num === 1) {
+        uncertain = num;
+        break;
+    }; 
+    uncertain = 12;
+}
+
+console.log(uncertain)
+
+// luckily you won't see any error here because TS automatically infers the undefined...but it is suggested to specify

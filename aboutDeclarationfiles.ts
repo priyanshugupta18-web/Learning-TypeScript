ef8474b60f05029c181712ad410895b479a92247
Priyanshu Gupta <priyanshuguptawebdev@gmail.com>
2026-07-29 02:12:02 +0530

    Add files via upload

---
+++ aboutDeclarationfiles.ts
@@ -0,0 +1,7 @@
+// TypeScript ke andar hamme jo bhi hints ya errors milte hai wo sab inhi declaration files se aa rahe hote hai, aap agar apne node_modules wale folder me jab TypeScript me jaoge to usme .d.ts karke aapko kai saari files milegi.
+
+// mostly saari library apne declaration files ko saath me ship karti hai...par kuchh libraries me aapko alag se inko install karna pad sakta hai uske liye ye command use kare:-
+//  npm i -D @types/lib-name
+
+// ++++++++++++++++++ ALL SET TO BEGIN +++++++++++++++++++++++++++
+
